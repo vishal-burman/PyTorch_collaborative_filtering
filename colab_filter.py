178e3625434efa86d3e160a529e65a92f20b4b5e
Vishal Burman <vishal.a.burman23@gmail.com>
2020-06-01 16:22:25 +0530

    Added Pytorch_lightning module

--- colab_filter.py
+++ colab_filter.py
@@ -6,6 +6,8 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
+from pytorch_lightning.core.lightning import LightningModule
+import pytorch_lightning as pl
 if torch.cuda.is_available():
     torch.backends.cudnn.deterministic = True
 
@@ -43,7 +45,7 @@ print(
 
 #########################################
 # Prepare Training and Validation data
-dataset = dataset.sample(frac=1, random_state=42)
+dataset = dataset.sample(frac=1)
 
 x = dataset[["user", "movie"]].values
 y = dataset["rating"].apply(lambda x: (
@@ -53,6 +55,11 @@ train_indices = int(0.9 * dataset.shape[0])
 x_train, x_val, y_train, y_val = (
     x[:train_indices], x[train_indices:], y[:train_indices], y[train_indices:])
 
+
+x_train=torch.from_numpy(x_train)
+y_train=torch.from_numpy(y_train).float()
+x_val=torch.from_numpy(x_val)
+y_val=torch.from_numpy(y_val).float()
 
 class DatasetColab(Dataset):
     def __init__(self, x, y):
@@ -69,22 +76,24 @@ class DatasetColab(Dataset):
         return X, Y
 
 
-training_set = DatasetColab(x_train, y_train)
-val_set = DatasetColab(x_val, y_val)
+#training_set = DatasetColab(x_train, y_train)
+#val_set = DatasetColab(x_val, y_val)
 
-training_loader = DataLoader(training_set, batch_size=64, shuffle=True)
-val_loader = DataLoader(val_set, batch_size=64, shuffle=False)
+#training_loader = DataLoader(training_set, batch_size=64, shuffle=True)
+#val_loader = DataLoader(val_set, batch_size=64, shuffle=False)
+EMBEDDING_SIZE = 50
 
 ###########################################
 # Creating the PyTorch model
 
 
-class RecommenderNet(nn.Module):
-    def __init__(self, num_users, num_movies, embedding_size):
+class RecommenderNet(LightningModule):
+    def __init__(self, num_users, num_movies, embedding_size ):
         super(RecommenderNet, self).__init__()
         self.num_users = num_users
         self.num_movies = num_movies
         self.embedding_size = embedding_size
+        self.loss=nn.BCELoss()
 
         self.user_embedding = nn.Embedding(num_users, embedding_size)
         self.user_bias = nn.Embedding(num_users, 1)
@@ -102,91 +111,101 @@ class RecommenderNet(nn.Module):
         x = nn.functional.sigmoid(x)
         return x
 
+    def prepare_data(self):
+        self.training_set=DatasetColab(x_train, y_train)
+        self.val_set=DatasetColab(x_val, y_val)
 
-EMBEDDING_SIZE = 50
+    def train_dataloader(self):
+        training_loader=DataLoader(self.training_set, batch_size=64, shuffle=True)
+        return training_loader
 
-model = RecommenderNet(num_users, num_movies, EMBEDDING_SIZE)
-model = model.to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    def val_dataloader(self):
+        val_loader=DataLoader(self.val_set, batch_size=64, shuffle=False)
+        return val_loader
 
+    def configure_optimizers(self):
+        optimizer=torch.optim.Adam(self.parameters(), lr=0.001 ,  eps=1e-7)
+        return optimizer
 
-def compute_val_loss(net, data_loader):
-    net.eval()
+    def training_step(self, batch, batch_idx):
+        features, targets=batch
+        targets=targets.unsqueeze(1)
+        outputs=self.forward(features)
+        cost=self.loss(outputs, targets)
+        log={'train_loss': cost}
+        return {'loss':cost, 'log': log}
 
-    cost_val = 0
-    with torch.no_grad():
-        for features, targets in data_loader:
-            features = features.to(device)
-            targets = targets.unsqueeze(1)
-            targets = targets.to(device)
-            logits = net(features)
-            cost_val += F.binary_cross_entropy(logits, targets)
-        return cost_val / 1000
+    def training_epoch_end(self, outputs):
+        avg_loss=torch.stack([x['loss'] for x in outputs]).mean()
+        tensorboard_logs={'avg_train_loss': avg_loss}
+        return {'avg_train_loss': avg_loss, 'log': tensorboard_logs, 'progress_bar': tensorboard_logs}
 
+    def validation_step(self, batch, batch_idx):
+        features, targets=batch
+        targets=targets.unsqueeze(1)
+        outputs=self.forward(features)
+        cost=self.loss(outputs, targets)
+        return {'val_loss': cost}
 
-num_epochs = 10
-for epoch in range(num_epochs):
-    cost_train = 0
-    model.train()
-    for idx, (features, targets) in enumerate(training_loader):
-        features = features.to(device)
-        targets = targets.unsqueeze(1)
-        targets = targets.to(device)
-
-        # Forward and Back propagation
-        preds = model(features)
-        cost = F.binary_cross_entropy(preds, targets)
-        cost_train += cost
-        optimizer.zero_grad()
-
-        # Update model parameters
-        cost.backward()
-        optimizer.step()
-
-    print("Epoch: {}/{} || Train Loss: {} || Val Loss: {}".format(epoch + 1,
-                                                                  num_epochs, cost_train / 1000, compute_val_loss(model, val_loader)))
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        log={'avg_loss': avg_loss}
+        return {'avg_loss': avg_loss, 'log': log}
 
 
-######################################################
-# Hacky...have to modify
-movie_df = pd.read_csv("ml-latest-small/movies.csv")
-df = dataset
-# Let us get a user and see the top recommendations.
-user_id = df.userId.sample(1).iloc[0]
-movies_watched_by_user = df[df.userId == user_id]
-movies_not_watched = movie_df[
-    ~movie_df["movieId"].isin(movies_watched_by_user.movieId.values)
-]["movieId"]
-movies_not_watched = list(
-    set(movies_not_watched).intersection(set(movie2movie_encoded.keys()))
-)
-movies_not_watched = [[movie2movie_encoded.get(x)] for x in movies_not_watched]
-user_encoder = user2user_encoded.get(user_id)
-user_movie_array = np.hstack(
-    ([[user_id]] * len(movies_not_watched), movies_not_watched)
-)
-user_movie_array = torch.from_numpy(user_movie_array).to(device)
-ratings = model(user_movie_array).flatten()
-top_ratings_indices = ratings.argsort()[-10:]
-recommended_movie_ids = [movie_encoded2movie.get(
-    movies_not_watched[x][0]) for x in top_ratings_indices]
 
-print("Showing recommendations for user: {}".format(user_id))
-print("====" * 9)
-print("Movies with high ratings from user")
-print("----" * 8)
-top_movies_user = (
-    movies_watched_by_user.sort_values(by="rating", ascending=False)
-    .head(5)
-    .movieId.values
-)
-movie_df_rows = movie_df[movie_df["movieId"].isin(top_movies_user)]
-for row in movie_df_rows.itertuples():
-    print(row.title, ":", row.genres)
+############################################################
+model=RecommenderNet(num_users, num_movies, EMBEDDING_SIZE)
+input_type=input("Type train to train the model or type predict to predict from pretrained model")
+if input_type=="train":
+    trainer=pl.Trainer(gpus=1, max_epoch=5)
+    trainer.fit(model)
+    trainer.save_checkpoint('example.ckpt')
+else:
+    device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    checkpoint=torch.load('example.ckpt', map_location= lambda storage, loc: storage)
+    model.load_state_dict(checkpoint['state_dict'])
+    model.eval()
 
-print("----" * 8)
-print("Top 10 movie recommendations")
-print("----" * 8)
-recommended_movies = movie_df[movie_df["movieId"].isin(recommended_movie_ids)]
-for row in recommended_movies.itertuples():
-    print(row.title, ":", row.genres)
+    movie_df = pd.read_csv("ml-latest-small/movies.csv")
+    df = dataset
+    # Let us get a user and see the top recommendations.
+    user_id=242
+    #user_id = df.userId.sample(1).iloc[0]
+    movies_watched_by_user = df[df.userId == user_id]
+    movies_not_watched = movie_df[
+        ~movie_df["movieId"].isin(movies_watched_by_user.movieId.values)
+    ]["movieId"]
+    movies_not_watched = list(
+        set(movies_not_watched).intersection(set(movie2movie_encoded.keys()))
+    )
+    movies_not_watched = [[movie2movie_encoded.get(x)] for x in movies_not_watched]
+    user_encoder = user2user_encoded.get(user_id)
+    user_movie_array = np.hstack(
+        ([[user_id]] * len(movies_not_watched), movies_not_watched)
+    )
+    user_movie_array = torch.from_numpy(user_movie_array)
+    ratings = model(user_movie_array).flatten()
+    top_ratings_indices = ratings.argsort()[-10:]
+    recommended_movie_ids = [movie_encoded2movie.get(
+        movies_not_watched[x][0]) for x in top_ratings_indices]
+
+    print("Showing recommendations for user: {}".format(user_id))
+    print("====" * 9)
+    print("Movies with high ratings from user")
+    print("----" * 8)
+    top_movies_user = (
+        movies_watched_by_user.sort_values(by="rating", ascending=False)
+        .head(5)
+        .movieId.values
+    )
+    movie_df_rows = movie_df[movie_df["movieId"].isin(top_movies_user)]
+    for row in movie_df_rows.itertuples():
+        print(row.title, ":", row.genres)
+
+    print("----" * 8)
+    print("Top 10 movie recommendations")
+    print("----" * 8)
+    recommended_movies = movie_df[movie_df["movieId"].isin(recommended_movie_ids)]
+    for row in recommended_movies.itertuples():
+        print(row.title, ":", row.genres)
